9d2b63b385dfe7eb1e81451bb9cd8f3ebd547d9f
Seunghwan Kim <kim03208@naver.com>
2021-06-01 23:58:54 +0900

    update page (add cheking login middlewares)

--- routes/page.js
+++ routes/page.js
@@ -1,20 +1,21 @@
 const express = require('express');
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
 
 const router = express.Router();
 
 router.use((req, res, next) => {
-    res.locals.user = null;                 // res.locals 로 값을 설정하는 이유 => 모든 템플릿 엔진에서 공통으로 사용하기 때문
+    res.locals.user = req.user;                 // res.locals 로 값을 설정하는 이유 => 모든 템플릿 엔진에서 공통으로 사용하기 때문
     res.locals.followerCount = 0;
     res.locals.followingCount = 0;
     res.locals.followerIdList = [];
     next();
 });
 
-router.get('/profile', (req, res) => {
+router.get('/profile', isLoggedIn, (req, res) => {
     res.render('profile', { title: '내 정보 - NodeBird '});
 });
 
-router.get('/join', (req, res) => {
+router.get('/join', isNotLoggedIn, (req, res) => {
     res.render('join', { title: "회원가입 - NodeBird"});
 });
 
