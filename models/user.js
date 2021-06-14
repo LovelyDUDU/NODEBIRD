59d22f2a23577380c415489669365315c5101c90
Seunghwan Kim <kim03208@naver.com>
2021-06-14 22:47:48 +0900

    add Like Table (M:N Post & User)

--- models/user.js
+++ models/user.js
@@ -51,5 +51,6 @@ module.exports = class User extends Sequelize.Model {
             as: 'Followings',
             through: 'Follow',
         })
+        db.User.belongsToMany(db.Post, { through: 'Like' });
     }
 }
